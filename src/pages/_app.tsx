2f5ce1252bc68ea53e7ce8f225cadecadb5502b3
Revion Labs <reviontechsol@gmail.com>
2025-12-01 12:25:58 +0700

    Create _app.tsx

---
+++ src/pages/_app.tsx
@@ -0,0 +1,12 @@
+import type { AppProps } from 'next/app';
+
+import '../styles/globals.css';
+import { AppLayout } from '../components/Layout/AppLayout';
+
+export default function MyApp({ Component, pageProps }: AppProps) {
+  return (
+    <AppLayout>
+      <Component {...pageProps} />
+    </AppLayout>
+  );
+}
